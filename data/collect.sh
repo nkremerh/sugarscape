b2d1dc59930f7d98e679fbb54890b7cca409f1c2
nkremerh@github.com <nkremerh@nd.edu>
2023-07-31 11:19:35 -0700

    Modifies data collection configs to set timesteps to 1000 and turn off debug printing Modifies data collector to run 100 seeds on 6 decision models

--- data/collect.sh
+++ data/collect.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-files=( default benthamNoLookaheadBinary benthamHalfLookaheadBinary benthamNoLookaheadTop benthamHalfLookaheadTop egoisticHalfLookaheadTop )
+files=( benthamNoLookaheadBinary benthamHalfLookaheadBinary benthamNoLookaheadTop benthamHalfLookaheadTop egoisticHalfLookaheadTop rawSugarscape )
 
 # Create working configs to avoid clobbering permanent configs
 for f in "${files[@]}"
@@ -8,22 +8,28 @@ do
     cp $f.config $f.json
 done
 
-n=1
+# Number of seeds to run
+n=100
+# Number of decision models to run per seed
+m=${#files[@]}
 for i in $( seq 1 $n )
 do
     # Generate a random seed
     seed=$RANDOM
     sedstr="s/\(\"seed\"\:\s\).*,/\1$seed,/g"
-    echo "Running simulation for random seed $seed ($i/$n)"
+    echo "Running simulations for random seed $seed ($i/$n)"
 
+    j=1
     for f in "${files[@]}"
     do
+        echo "Running decision model $f ($j/$m)"
         # Apply seed to config file
         sed -i $sedstr ./$f.json
         # Run simulation for configs and rename resulting log
         python ../sugarscape.py --conf $f.json > $f$i.log
         #python ../logparse.py --log log.json >> $f$i.log
         mv log.json $f$i.json
+        j=$((j+1))
     done
 done
 
