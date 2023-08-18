189e549b49036fbbbac42321b3d6e23747502393
Willem Hueffed <whueffed@seattleu.edu>
2023-08-17 17:20:03 -0700

    Adds scraping script to simulation Adds plotting scripts to simulation Modifies makefile to to support automated graphing via 'make plots' Removes example files from plots/

---
+++ plots/generate_line_graphs.sh
@@ -0,0 +1,8 @@
+plottingScripts=( plot_agent_wealth_collected.plg plot_agent_wealth_total.plg plot_mean_time_to_live_age_limited.plg plot_mean_time_to_live.plg plot_percent_population_growth.plg plot_population_per_timestep.plg plot_starvation_deaths.plg plot_total_wealth.plg plot_wealth_collected.plg plot_agent_wealth_total_div_by_pop.plg plot_agent_wealth_collected_div_by_pop.plg)
+
+python "parse_logs_for_graphs.py" -p "../data/" -l line_graphs.dat
+
+for ((graphIndex=0; graphIndex<"${#plottingScripts[@]}"; graphIndex++))
+do
+    gnuplot -c ${plottingScripts[$graphIndex]} 'line_graphs.dat'
+done
