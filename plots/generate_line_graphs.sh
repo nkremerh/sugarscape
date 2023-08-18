fc662b34e9667dfb398ade5720d8362de0d376d0
Willem Hueffed <whueffed@seattleu.edu>
2023-08-17 18:41:41 -0700

    Fixes plot names being camelCased instead of snake_cased Adds scripts to generate agent total metabolism divided by enviroment wealth created Fixes graph y axis being incorrectly labelled

--- plots/generate_line_graphs.sh
+++ plots/generate_line_graphs.sh
@@ -1,4 +1,4 @@
-plottingScripts=( plot_agent_wealth_collected.plg plot_agent_wealth_total.plg plot_mean_time_to_live_age_limited.plg plot_mean_time_to_live.plg plot_percent_population_growth.plg plot_population_per_timestep.plg plot_starvation_deaths.plg plot_total_wealth.plg plot_wealth_collected.plg plot_agent_wealth_total_div_by_pop.plg plot_agent_wealth_collected_div_by_pop.plg)
+plottingScripts=( plot_agent_wealth_collected.plg plot_agent_wealth_total.plg plot_mean_time_to_live_age_limited.plg plot_mean_time_to_live.plg plot_percent_population_growth.plg plot_population_per_timestep.plg plot_starvation_deaths.plg plot_total_wealth.plg plot_wealth_collected.plg plot_agent_wealth_total_div_by_pop.plg plot_agent_wealth_collected_div_by_pop.plg plot_agent_tot_met_div_by_env_wealth_created.plg)
 
 python "parse_logs_for_graphs.py" -p "../data/" -l line_graphs.dat
 
