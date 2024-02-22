05f82b96c3415198634e9ce9600e43f012784c47
colinhanrahan <118491522+colinhanrahan@users.noreply.github.com>
2024-02-21 18:41:07 -0800

    Moves len() calls outside loop

--- ethics.py
+++ ethics.py
@@ -17,7 +17,8 @@ class Bentham(agent.Agent):
         globalMaxWealth = cell.environment.globalMaxSugar + cell.environment.globalMaxSpice
         cellValue = 0
         selfishnessFactor = self.selfishnessFactor
-        futureNeighborhood = self.findNeighborhood(cell)
+        neighborhoodSize = len(self.neighborhood)
+        futureNeighborhoodSize = len(self.findNeighborhood(cell))
         for neighbor in self.neighborhood:
             # Timesteps to reach cell, currently 1 since agents only plan for the current timestep
             timestepDistance = 1
@@ -34,8 +35,8 @@ class Bentham(agent.Agent):
             futureDuration = futureDuration / cellMaxSiteWealth if cellMaxSiteWealth > 0 else 0
             futureIntensity = cellNeighborWealth / (globalMaxWealth * 4)
             # Assuming agent can only see in four cardinal directions
-            extent = len(self.neighborhood) / (neighbor.vision * 4) if neighbor.vision > 0 else 1
-            futureExtent = len(futureNeighborhood) / (neighbor.vision * 4) if neighbor.vision > 0 and self.lookahead != None else 1
+            extent = neighborhoodSize / (neighbor.vision * 4) if neighbor.vision > 0 else 1
+            futureExtent = futureNeighborhoodSize / (neighbor.vision * 4) if neighbor.vision > 0 and self.lookahead != None else 1
             neighborValueOfCell = 0
             # If not the agent moving, consider these as opportunity costs
             if neighbor != self and cell != neighbor.cell and self.selfishnessFactor < 1:
