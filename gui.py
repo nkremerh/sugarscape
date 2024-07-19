d0fa85e06ebc702d6f492f58b68c60e1452bca6b
colinhanrahan <colinhanrahan9@gmail.com>
2024-07-18 18:23:10 -0700

    Simplifies mean calculation

--- gui.py
+++ gui.py
@@ -27,7 +27,7 @@ class GUI:
         self.lastSelectedAgentColor = None
         self.lastSelectedEnvironmentColor = None
         self.activeColorOptions = {"agent": None, "environment": None}
-        self.meanAgentMetabolism = (sum(self.sugarscape.configuration["agentSugarMetabolism"]) + sum(self.sugarscape.configuration["agentSpiceMetabolism"])) / 2
+        self.meanAgentMetabolism = sum(self.sugarscape.configuration["agentSugarMetabolism"] + self.sugarscape.configuration["agentSpiceMetabolism"]) / 2
         self.meanAgentMovement = sum(self.sugarscape.configuration["agentMovement"]) / 2
         self.meanAgentVision = sum(self.sugarscape.configuration["agentVision"]) / 2
         self.highlightedCell = None
