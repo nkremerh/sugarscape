458110fe22ef6bdfda5fc4efcf54aca481221ef9
Anna Muller <annaymuller99@gmail.com>
2024-07-18 21:39:28 -0700

    Adds Analysis class from log

--- analysis.py
+++ analysis.py
@@ -2,59 +2,72 @@ import csv
 import json
 import sys
 
-# Will only produce results for the first run in a CSV file
-def analyzeTribalAssimilation(log):
-    tribeSummary = {}
-    numTribes = 0
+class Analysis:
+    def __init__(self, log):
+        self.log = log
+        self.numTribes = int(self.log[0]["totalTribes"])
+        self.assimilationPercentage = 100 / (self.numTribes - 1)
+        self.tribeSummary = {"dominantTribe": -1, "timestep": -1}
+
+    def analyzeTribalAssimilation(self):
+        for day in log:
+            majorityPercent = float(day["tribePopulationPercentage"])
+            if self.isTribeDominant(majorityPercent) == True:
+                self.tribeSummary["dominantTribe"] = int(day["majorityTribe"])
+                self.tribeSummary["timestep"] = day["timestep"]
+                break
+        if self.tribeSummary["dominantTribe"] < 0:
+            print("There was no clear dominant tribe during this run.")
+        else:
+            print(f"Tribe {self.tribeSummary['dominantTribe']} is over {int(self.assimilationPercentage)}% of the population at timestep {self.tribeSummary['timestep']}.")
+
+    def isTribeDominant(self, percent):
+        if percent > self.assimilationPercentage / 100:
+            return True
+        return False
+
+    def __str__(self):
+        return self.tribeSummary
+
+def loadCSV(filename):
+    file = open(filename)
+    log = list(csv.DictReader(file))
     for day in log:
-        numTribes = int(day["totalTribes"])
-        populationPercent = float(day["tribePopulationPercentage"])
-        if isTribeDominant(numTribes, populationPercent) == True:
-            tribeSummary["dominantTribe"] = day["majorityTribe"]
-            tribeSummary["timestep"] = day["timestep"]
-            break
-    print(f"Tribe {tribeSummary['dominantTribe']} is at least {int(100 / (numTribes - 1))}% of the population at timestep {tribeSummary['timestep']}")
-
-def isTribeDominant(numTribes, percent):
-    dominantPercent = (100 / (numTribes - 1)) / 100
-    if percent > dominantPercent:
-        return True
-    return False
-
-def loadCSVFile(fileName):
-    logFile = open(fileName)
-    log = csv.DictReader(logFile)
+        if list(day.keys()) == list(day.values()):
+            print("invalid log file. Cannot have more than one entry per log.")
+            exit(1)
     return log
 
-# Will only parse if there is only one run logged in the file
-def loadJSONFile(filename):
-    logFile = open(fileName)
-    log = json.load(logFile)
+def loadJSON(filename):
+    try:
+        file = open(filename)
+        log = json.load(file)
+    except:
+        print("Invalid log file. Cannot have more than one entry per log.")
+        exit(1)
     return log
 
-def parseFile(filename):
-    split = filename.split(".")
-    format = split[-1]
-    return format
+def parseFile(fileName):
+    split = fileName.split(".")
+    fileFormat = split[-1]
+    return fileFormat
 
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print("Usage: analysis.py [logfile]")
+    if len(sys.argv) != 2:
+        print("Usage: python analysis.py [logfile]")
         exit(1)
 
-    fileName = sys.argv[-1]
-    fileFormat = parseFile(fileName)
-
+    file = sys.argv[1]
+    fileFormat = parseFile(file)
     log = None
-
     if fileFormat == "csv":
-        log = loadCSVFile(fileName)
+        log = loadCSV(file)
     elif fileFormat == "json":
-        log = loadJSONFile(fileName)
-    else:
-        print("Unsupported file format.")
+        log = loadJSON(file)
+
+    if log == None:
+        print("No log found.")
         exit(1)
-
-    analyzeTribalAssimilation(log)
-
-    exit(0)
+    else:
+        A = Analysis(log)
+        A.analyzeTribalAssimilation()
