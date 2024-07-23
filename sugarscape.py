d16710514d3222ba7c58cdfa3ade11b24be63f7a
colinhanrahan <118491522+colinhanrahan@users.noreply.github.com>
2024-07-23 14:02:56 -0700

    Fixes bug when typechecking empty lists during configuration validation (#108)

--- sugarscape.py
+++ sugarscape.py
@@ -1000,6 +1000,8 @@ def verifyConfiguration(configuration):
     negativeFlag = 0
     for configName, configValue in configuration.items():
         if isinstance(configValue, list):
+            if len(configValue) == 0:
+                continue
             configType = type(configValue[0])
             if configName != "environmentPollutionDiffusionTimeFrame" and configName != "environmentPollutionTimeFrame":
                 configValue.sort()
