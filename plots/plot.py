1fbdce80abf92321c1be594dc8f2560ea3d850a2
nkremerh <nkremerh@users.noreply.github.com>
2025-06-16 14:05:03 -0700

    Adds mechanism to switch between plotting by mean or by median

--- plots/plot.py
+++ plots/plot.py
@@ -1,124 +1,45 @@
 import csv
 import getopt
 import json
+import math
 import matplotlib.pyplot
 import matplotlib.ticker
 import os
 import re
 import sys
 
-datacols = []
-
-def parseDataset(path, dataset, totalTimesteps, skipExtinct=False):
-    encodedDir = os.fsencode(path)
-    for file in os.listdir(encodedDir):
-        filename = os.fsdecode(file)
-        if not (filename.endswith(".json") or filename.endswith(".csv")):
-            continue
-        filePath = path + filename
-        fileDecisionModel = re.compile(r"^([A-z]*)(\d*)\.(json|csv)")
-        fileSearch = re.search(fileDecisionModel, filename)
-        if fileSearch == None:
-            continue
-        model = fileSearch.group(1)
-        if model not in dataset:
-            continue
-        seed = fileSearch.group(2)
-        log = open(filePath)
-        print("Reading log {0}".format(filePath))
-        rawData = None
-        if filename.endswith(".json"):
-            rawData = json.loads(log.read())
-        else:
-            rawData = list(csv.DictReader(log))
-
-        if int(rawData[-1]["population"]) == 0:
-            dataset[model]["died"] += 1
-            if skipExtinct == True:
-                continue
-        elif int(rawData[-1]["population"]) <= int(rawData[0]["population"]):
-            dataset[model]["worse"] += 1
-        else:
-            dataset[model]["better"] += 1
-
-        dataset[model]["runs"] += 1
-        i = 1
-        for item in rawData:
-            if int(item["timestep"]) > totalTimesteps:
-                break
-            if int(item["timestep"]) > dataset[model]["timesteps"]:
-                dataset[model]["timesteps"] += 1
-
-            for entry in item:
-                if entry in ["agentWealths", "agentTimesToLive", "agentTimesToLiveAgeLimited", "agentTotalMetabolism"]:
-                    continue
-                if entry not in datacols:
-                    datacols.append(entry)
-                if entry not in dataset[model]["metrics"]:
-                    dataset[model]["metrics"][entry] = [0 for j in range(totalTimesteps + 1)]
-                if item[entry] == "None":
-                    item[entry] = 0
-                dataset[model]["metrics"][entry][i-1] += float(item[entry])
-            i += 1
-    for model in dataset:
-        if dataset[model]["runs"] == 0:
-            print(f"No simulation runs found for the {model} decision model")
-    return dataset
-
 def findMeans(dataset):
     print("Finding mean values across {0} timesteps".format(totalTimesteps))
     for model in dataset:
-        for column in datacols:
+        for column in dataset[model]["metrics"]:
             for i in range(len(dataset[model]["metrics"][column])):
-                if column not in dataset[model]["means"]:
-                    dataset[model]["means"][column] = [0 for j in range(totalTimesteps + 1)]
-                dataset[model]["means"][column][i] = dataset[model]["metrics"][column][i] / dataset[model]["runs"]
+                if column not in dataset[model]["aggregates"]:
+                    dataset[model]["aggregates"][column] = [0 for j in range(totalTimesteps + 1)]
+                dataset[model]["aggregates"][column][i] = dataset[model]["metrics"][column][i] / dataset[model]["runs"]
     return dataset
 
-def parseOptions():
-    commandLineArgs = sys.argv[1:]
-    shortOptions = "c:p:s:t:h"
-    longOptions = ("conf=", "path=", "help", "skip")
-    options = {"config": None, "path": None, "skip": False}
-    try:
-        args, vals = getopt.getopt(commandLineArgs, shortOptions, longOptions)
-    except getopt.GetoptError as err:
-        print(err)
-        exit(0)
-    for currArg, currVal in args:
-        if currArg in ("-c", "--conf"):
-            if currVal == "":
-                print("No config file provided.")
-                printHelp()
-            options["config"] = currVal
-        elif currArg in ("-p", "--path"):
-            options["path"] = currVal
-            if currVal == "":
-                print("No dataset path provided.")
-                printHelp()
-        elif currArg in ("-h", "--help"):
-            printHelp()
-        elif currArg in ("-s", "--skip"):
-            options["skip"] = True
-    flag = 0
-    if options["path"] == None:
-        print("Dataset path required.")
-        flag = 1
-    if options["config"] == None:
-        print("Configuration file path required.")
-        flag = 1
-    if flag == 1:
-        printHelp()
-    return options
-
-def printHelp():
-    print("Usage:\n\tpython plot.py --path /path/to/data --conf /path/to/config > results.dat\n\nOptions:\n\t-c,--conf\tUse the specified path to configurable settings file.\n\t-p,--path\tUse the specified path to find dataset JSON files.\n\t-s,--skip\tSkip including extinct societies in produced graphs.\n\t-h,--help\tDisplay this message.")
-    exit(0)
-
-def printSummaryStats(dataset):
-    print("Model population performance:\n{0:^30} {1:^5} {2:^5} {3:^5}".format("Decision Model", "Extinct", "Worse", "Better"))
+def findMedians(dataset):
+    print("Finding median values across {0} timesteps".format(totalTimesteps))
     for model in dataset:
-        print("{0:^30}: {1:^5} {2:^5} {3:^5}".format(model, dataset[model]["died"], dataset[model]["worse"], dataset[model]["better"]))
+        for column in dataset[model]["metrics"]:
+            for i in range(len(dataset[model]["metrics"][column])):
+                sortedColumn = sorted(dataset[model]["metrics"][column][i])
+                columnLength = len(sortedColumn)
+                midpoint = math.floor(columnLength / 2)
+                median = sortedColumn[midpoint]
+                quartile = math.floor(columnLength / 4)
+                firstQuartile = sortedColumn[quartile]
+                thirdQuartile = sortedColumn[midpoint + quartile]
+                if columnLength % 2 == 0:
+                    median = round((sortedColumn[midpoint - 1] + median) / 2, 2)
+                    firstQuartile = round((sortedColumn[quartile - 1] + firstQuartile) / 2, 2)
+                    thirdQuartile = round((sortedColumn[(midpoint + quartile) - 1] + thirdQuartile) / 2, 2)
+                if column not in dataset[model]["aggregates"]:
+                    dataset[model]["aggregates"][column] = [0 for j in range(totalTimesteps + 1)]
+                    dataset[model]["firstQuartiles"][column] = [0 for j in range(totalTimesteps + 1)]
+                    dataset[model]["thirdQuartiles"][column] = [0 for j in range(totalTimesteps + 1)]
+                dataset[model]["aggregates"][column][i] = median
+    return dataset
 
 def generatePlots(config, models, totalTimesteps, dataset, experimentalGroup=None):
     if "deaths" in config["plots"]:
@@ -188,19 +109,124 @@ def generateSimpleLinePlot(models, dataset, totalTimesteps, outfile, column, lab
         if experimentalGroup != None:
             controlGroupColumn = "control" + column[0].upper() + column[1:]
             controlGroupLabel = f"Control {modelStrings[modelString]}"
-            y = [dataset[model]["means"][controlGroupColumn][i] for i in range(totalTimesteps + 1)]
+            y = [dataset[model]["aggregates"][controlGroupColumn][i] for i in range(totalTimesteps + 1)]
             axes.plot(x, y, color=colors[model], label=controlGroupLabel)
             experimentalGroupColumn = experimentalGroup + column[0].upper() + column[1:]
             experimentalGroupLabel = experimentalGroup[0].upper() + experimentalGroup[1:] + f" {modelStrings[modelString]}"
-            y = [dataset[model]["means"][experimentalGroupColumn][i] for i in range(totalTimesteps + 1)]
+            y = [dataset[model]["aggregates"][experimentalGroupColumn][i] for i in range(totalTimesteps + 1)]
             axes.plot(x, y, color=colors[model], label=experimentalGroupLabel, linestyle="dotted")
         else:
-            y = [dataset[model]["means"][column][i] for i in range(totalTimesteps + 1)]
+            y = [dataset[model]["aggregates"][column][i] for i in range(totalTimesteps + 1)]
             axes.plot(x, y, color=colors[modelString], label=modelStrings[modelString])
         axes.legend(loc=positioning, labelspacing=0.1, frameon=False, fontsize=16)
     if percentage == True:
         axes.yaxis.set_major_formatter(matplotlib.ticker.PercentFormatter())
     figure.savefig(outfile, format="pdf", bbox_inches="tight")
+
+def parseDataset(path, dataset, totalTimesteps, statistic, skipExtinct=False):
+    encodedDir = os.fsencode(path)
+    for file in os.listdir(encodedDir):
+        filename = os.fsdecode(file)
+        if not (filename.endswith(".json") or filename.endswith(".csv")):
+            continue
+        filePath = path + filename
+        fileDecisionModel = re.compile(r"^([A-z]*)(\d*)\.(json|csv)")
+        fileSearch = re.search(fileDecisionModel, filename)
+        if fileSearch == None:
+            continue
+        model = fileSearch.group(1)
+        if model not in dataset:
+            continue
+        seed = fileSearch.group(2)
+        log = open(filePath)
+        print("Reading log {0}".format(filePath))
+        rawData = None
+        if filename.endswith(".json"):
+            rawData = json.loads(log.read())
+        else:
+            rawData = list(csv.DictReader(log))
+
+        if int(rawData[-1]["population"]) == 0:
+            dataset[model]["died"] += 1
+            if skipExtinct == True:
+                continue
+        elif int(rawData[-1]["population"]) <= int(rawData[0]["population"]):
+            dataset[model]["worse"] += 1
+        else:
+            dataset[model]["better"] += 1
+
+        dataset[model]["runs"] += 1
+        i = 1
+        for item in rawData:
+            if int(item["timestep"]) > totalTimesteps:
+                break
+            if int(item["timestep"]) > dataset[model]["timesteps"]:
+                dataset[model]["timesteps"] += 1
+
+            for entry in item:
+                if entry in ["agentWealths", "agentTimesToLive", "agentTimesToLiveAgeLimited", "agentTotalMetabolism"]:
+                    continue
+                if entry not in dataset[model]["metrics"]:
+                    if statistic == "mean":
+                        dataset[model]["metrics"][entry] = [0 for j in range(totalTimesteps + 1)]
+                    elif statistic == "median":
+                        dataset[model]["metrics"][entry] = [[] for j in range(totalTimesteps + 1)]
+                if item[entry] == "None":
+                    item[entry] = 0
+                if statistic == "mean":
+                    dataset[model]["metrics"][entry][i-1] += float(item[entry])
+                elif statistic == "median":
+                    dataset[model]["metrics"][entry][i-1].append(float(item[entry]))
+            i += 1
+    for model in dataset:
+        if dataset[model]["runs"] == 0:
+            print(f"No simulation runs found for the {model} decision model")
+    return dataset
+
+def parseOptions():
+    commandLineArgs = sys.argv[1:]
+    shortOptions = "c:p:s:t:h"
+    longOptions = ("conf=", "path=", "help", "skip")
+    options = {"config": None, "path": None, "skip": False}
+    try:
+        args, vals = getopt.getopt(commandLineArgs, shortOptions, longOptions)
+    except getopt.GetoptError as err:
+        print(err)
+        exit(0)
+    for currArg, currVal in args:
+        if currArg in ("-c", "--conf"):
+            if currVal == "":
+                print("No config file provided.")
+                printHelp()
+            options["config"] = currVal
+        elif currArg in ("-p", "--path"):
+            options["path"] = currVal
+            if currVal == "":
+                print("No dataset path provided.")
+                printHelp()
+        elif currArg in ("-h", "--help"):
+            printHelp()
+        elif currArg in ("-s", "--skip"):
+            options["skip"] = True
+    flag = 0
+    if options["path"] == None:
+        print("Dataset path required.")
+        flag = 1
+    if options["config"] == None:
+        print("Configuration file path required.")
+        flag = 1
+    if flag == 1:
+        printHelp()
+    return options
+
+def printHelp():
+    print("Usage:\n\tpython plot.py --path /path/to/data --conf /path/to/config > results.dat\n\nOptions:\n\t-c,--conf\tUse the specified path to configurable settings file.\n\t-p,--path\tUse the specified path to find dataset JSON files.\n\t-s,--skip\tSkip including extinct societies in produced graphs.\n\t-h,--help\tDisplay this message.")
+    exit(0)
+
+def printSummaryStats(dataset):
+    print("Model population performance:\n{0:^30} {1:^5} {2:^5} {3:^5}".format("Decision Model", "Extinct", "Worse", "Better"))
+    for model in dataset:
+        print("{0:^30}: {1:^5} {2:^5} {3:^5}".format(model, dataset[model]["died"], dataset[model]["worse"], dataset[model]["better"]))
 
 if __name__ == "__main__":
     options = parseOptions()
@@ -214,19 +240,27 @@ if __name__ == "__main__":
     config = config["dataCollectionOptions"]
     totalTimesteps = config["plotTimesteps"]
     models = config["decisionModels"]
+    statistic = config["plotStatistic"]
     dataset = {}
     for model in models:
         modelString = model
         if type(model) == list:
             modelString = '_'.join(model)
-        dataset[modelString] = {"runs": 0, "died": 0, "worse": 0, "better": 0, "timesteps": 0, "means": {}, "metrics": {}}
+        dataset[modelString] = {"runs": 0, "died": 0, "worse": 0, "better": 0, "timesteps": 0, "aggregates": {}, "firstQuartiles": {}, "thirdQuartiles": {}, "metrics": {}}
 
     if not os.path.exists(path):
-        print("Path {0} not recognized.".format(path))
+        print(f"Path {path} not recognized.")
         printHelp()
 
-    dataset = parseDataset(path, dataset, totalTimesteps, skipExtinct)
-    dataset = findMeans(dataset)
+    dataset = parseDataset(path, dataset, totalTimesteps, statistic, skipExtinct)
+    if statistic == "mean":
+        dataset = findMeans(dataset)
+    elif statistic == "median":
+        dataset = findMedians(dataset)
+    else:
+        print(f"Plotting statistic {statistic} not recognized.")
+        printHelp()
+
     generatePlots(config, models, totalTimesteps, dataset, experimentalGroup)
     printSummaryStats(dataset)
     exit(0)
